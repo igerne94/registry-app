d35cd05c5a2e166b73b33fb5ca880e3c8e675f57
Hanna <igerne94@gmail.com>
2021-06-20 12:10:42 +0200

    weather

---
+++ src/routes/Weather.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+function Weather(){
+    return (
+        <>
+        </>
+    )
+}
+export default Weather;
